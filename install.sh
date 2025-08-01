a248a87700b694d7d6da304ef71429613f9fdfa5
Marcos Cannabrava <mpcannabrava@gmail.com>
2025-08-01 10:04:49 -0700

    use reflector to update mirrorlist so installation goes faster

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ fi
 log_purple "installing base packages..."
 log_purple "------------------------------------"
 if ! command -v yay &> /dev/null; then
-    sudo pacman -Syu --needed git base-devel yay
+    sudo pacman -Syu --needed git base-devel
 else
     log_green "base packages already installed."
 fi
@@ -20,13 +20,15 @@ log_red "minor: pacman configuration update failed..."
 
 log_purple "updating package database..."
 log_purple "------------------------------------"
-sudo pacman -Syyu && sudo pacman -Fy
+sudo pacman -S reflector yay # install yay aur helper
+reflector --latest 20 --sort rate --save /etc/pacman.d/mirrorlist # sort mirrors by speed
+sudo yay -Syu
 
 
 log_purple "installing packages from pkg.list..."
 log_purple "------------------------------------"
 PKG_LIST="$(grep -vE '^#|^$' pkg.list | cut -d'|' -f1 | xargs)"
-yay -S --needed --noconfirm $PKG_LIST
+sudo yay -S --needed --noconfirm $PKG_LIST
 
 
 # todo:
