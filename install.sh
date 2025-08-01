e175061dbd0b7ad407662e3a61c899155e968a34
Marcos Cannabrava <mpcannabrava@gmail.com>
2025-08-01 11:02:08 -0700

    idempotency

--- install.sh
+++ install.sh
@@ -1,38 +1,32 @@
 CUR_DIR=`pwd`
 
 if [ -f utils.sh ]; then source utils.sh;
-else echo "utils.sh not found."; exit 1
-fi
+else echo "utils.sh not found."; exit 1; fi
 
-log_purple "installing base packages..."
-log_purple "------------------------------------"
 if ! command -v yay &> /dev/null; then
+    log_purple "installing base packages..."
+    log_purple "------------------------------------"
     sudo pacman -Syu --needed git base-devel
-else
-    log_green "base packages already installed."
+    log_purple "configuring pacman..."
+    log_purple "------------------------------------"
+
+    ./config_pacman.sh && \
+    log_green "pacman configured successfully." || \
+    log_red "minor: pacman configuration update failed..."
+
+    log_purple "configuring mirrors..."
+    log_purple "------------------------------------"
+    sudo pacman -S reflector
+    reflector --latest 20 --sort rate --save /etc/pacman.d/mirrorlist # sort mirrors by speed
+
+    log_purple "installing yay..."
+    log_purple "------------------------------------"
+    cd $HOME
+    git clone https://aur.archlinux.org/yay-bin.git .yay-bin
+    cd .yay-bin
+    makepkg -si
+    cd $CUR_DIR
 fi
-
-
-log_purple "configuring pacman..."
-log_purple "------------------------------------"
-./config_pacman.sh && \
-log_green "pacman configured successfully." || \
-log_red "minor: pacman configuration update failed..."
-
-
-log_purple "configuring mirrors..."
-log_purple "------------------------------------"
-sudo pacman -S reflector
-reflector --latest 20 --sort rate --save /etc/pacman.d/mirrorlist # sort mirrors by speed
-
-
-log_purple "installing yay..."
-log_purple "------------------------------------"
-cd $HOME
-git clone https://aur.archlinux.org/yay-bin.git .yay-bin
-cd .yay-bin
-makepkg -si
-cd $CUR_DIR
 
 
 log_purple "updating package database..."
