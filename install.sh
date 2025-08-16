15e2c9a139ddca665aaa4fc7a304999c3f4f9a8b
Marcos Cannabrava <mpcannabrava@gmail.com>
2025-08-15 20:24:59 -0700

    util functions and copycat starter dotfiles

--- install.sh
+++ install.sh
@@ -3,46 +3,57 @@ CUR_DIR=`pwd`
 if [ -f utils.sh ]; then source utils.sh;
 else echo "utils.sh not found."; exit 1; fi
 
+log_purple "#######################################"
+log_purple "#### package manager configuration ####"
+log_purple "#######################################\n"
 if ! command -v yay &> /dev/null; then
     log_purple "installing base packages..."
-    log_purple "------------------------------------"
     sudo pacman -Syu --needed git base-devel
-    log_purple "configuring pacman..."
-    log_purple "------------------------------------"
 
+    log_purple "configuring pacman..."
     ./config_pacman.sh && \
-    log_green "pacman configured successfully." || \
-    log_red "minor: pacman configuration update failed..."
+    log_green "pacman configured successfully." || log_yellow "pacman already configured."
 
     log_purple "configuring mirrors..."
-    log_purple "------------------------------------"
     sudo pacman -S reflector
     reflector --latest 20 --sort rate --save /etc/pacman.d/mirrorlist # sort mirrors by speed
 
     log_purple "installing yay..."
-    log_purple "------------------------------------"
     cd $HOME
-    git clone https://aur.archlinux.org/yay-bin.git .yay-bin
-    cd .yay-bin
-    makepkg -si
+    git clone https://aur.archlinux.org/yay-bin.git .yay-bin && \
+    cd .yay-bin && \
+    makepkg -si && \
+    log_green "yay installed successfully." || \
+    (log_red "failed to install yay. exiting... please install manually."; exit 1)
     cd $CUR_DIR
 fi
 
 
+log_purple "#######################################"
+log_purple "######### installing packages #########"
+log_purple "#######################################\n"
 log_purple "updating package database..."
-log_purple "------------------------------------"
 sudo yay -Syu
 
-
 log_purple "installing packages from pkg.list..."
-log_purple "------------------------------------"
 PKG_LIST="$(grep -vE '^#|^$' pkg.list | cut -d'|' -f1 | xargs)"
 sudo yay -S --needed --noconfirm $PKG_LIST
 
 
-# todo:
-# install fonts
-# symlink config files
+log_purple "#######################################"
+log_purple "######### symlinking dotfiles #########"
+log_purple "#######################################\n"
+for file in $(find config -type f); do
+    target="$HOME/.$file"
+    if [ -e "$target" ]; then
+        backup "$target"
+    fi
+    mkdir -p "$(dirname "$target")"
+    ln -s "$CUR_DIR/$file" "$target"
+    log_green "linked: $target"
+done
 
-log_green "installation complete!"
-log_green "------------------------------------"
+
+log_green "##########################################"
+log_green "######### INSTALLATION COMPLETE! #########"
+log_green "##########################################"
--- utils.sh
+++ utils.sh
@@ -23,12 +23,27 @@ function log_green() {
     echo -e "${BOLD}${GREEN}$1${RESET}"
 }
 
+function log_yellow() {
+    echo -e "${BOLD}${YELLOW}$1${RESET}"
+}
+
 function log_red() {
     echo -e "${BOLD}${RED}$1${RESET}"
 }
 
+
 function log_blue_on_yellow() {
     echo -e "${BOLD}${BLUE}${BG_YELLOW}$1${RESET}"
+}
+
+function backup() {
+    local src="$1"
+    local dest="${2:-${src}.bkp}"
+    if [[ -z "$src" ]]; then log_red "No source file specified for backup."; return 1; fi
+
+    cp -a -- "$src" "$dest" && \
+    log_green "'$src' backed up." || \
+    (log_red "Failed to backup '$src'."; return 1)
 }
 
 cat <<EOF
