c8542108488f477e7ed74369a0f3479345c55579
Marcos Cannabrava <mpcannabrava@gmail.com>
2026-05-20 16:57:56 -0700

    make install scripts executable

--- install/splashscreen.sh
+++ install/splashscreen.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-mv /usr/share/plymouth/themes/omarchy/logo.png /usr/share/plymouth/themes/omarchy/logo.png.bkp
-cp logo.png /usr/share/plymouth/themes/omarchy
-sudo limine-update
+sudo mv /usr/share/plymouth/themes/omarchy/logo.png /usr/share/plymouth/themes/omarchy/logo.png.bkp
+sudo cp logo.png /usr/share/plymouth/themes/omarchy
