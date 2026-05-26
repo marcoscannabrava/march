451bd534b12c6d92697158d25a081f6dd4115425
Marcos Cannabrava <mpcannabrava@gmail.com>
2026-05-26 11:01:57 -0700

    fix splashscreen logo

--- install/splashscreen.sh
+++ install/splashscreen.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 sudo mv /usr/share/plymouth/themes/omarchy/logo.png /usr/share/plymouth/themes/omarchy/logo.png.bkp
 sudo cp logo.png /usr/share/plymouth/themes/omarchy
+
+# same for /home/marcos/.local/share/omarchy/default/plymouth/logo.png
+sudo mv /home/marcos/.local/share/omarchy/default/plymouth/logo.png /home/marcos/.local/share/omarchy/default/plymouth/logo.png.bkp
+sudo cp logo.png /home/marcos/.local/share/omarchy/default/plymouth
